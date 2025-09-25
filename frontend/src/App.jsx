bbdda6f782e1b738438da2d0ce4c1623aede696d
L-HITS <leechan753@gmail.com>
2025-09-25 22:06:08 +0900

    페이지 app.jsx에 등록

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Header from "./components/Header";
 import Chat from "./pages/Chat";
+import Test from "./pages/test";
 
 // 페이지 라우터가 설정된 최상위 컴포넌트
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
