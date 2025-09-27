74a2f835ac29deb4568d1935b168276fe3f8a396
Park Jeongmin <pjm00527@gmail.com>
2025-09-27 21:27:22 +0900

    메인페이지 라우팅 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
-import Header from "./components/Header";
-import Chat from "./pages/Chat";
-import Test from "./pages/test";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Test from './pages/test';
+import MainPage from './pages/MainPage';
 // 페이지 라우터가 설정된 최상위 컴포넌트
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/test" element={<Test />} />
       </Routes>
