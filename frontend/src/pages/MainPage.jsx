b8ebddf1a86c2138b82e11d5cee4326d9fa05aea
K-TJ-125 <ktj16489@naver.com>
2025-09-28 13:14:45 +0900

    최종 수정

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./MainPage.css";
+import TalentTest from "./TalentTest";
 
 function MainPage() {
   const [talentInfo, setTalentInfo] = useState(null);
